45253906ad36fef4c8d1fa91b7e74bca1828b70c
Vitaliy Rudkovskiy <>
2025-12-04 15:13:27 +0100

    feat: Add theme colours

--- composeApp/src/commonMain/kotlin/one/vitaliy/whatscooking/App.kt
+++ composeApp/src/commonMain/kotlin/one/vitaliy/whatscooking/App.kt
@@ -1,35 +1,42 @@
 package one.vitaliy.whatscooking
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import one.vitaliy.whatscooking.categories.list.CategoriesPage
+import one.vitaliy.whatscooking.di.initKoin
 import one.vitaliy.whatscooking.randommeal.RandomMealScreen
+import one.vitaliy.whatscooking.ui.theme.WhatsCookingTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinApplication
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        Scaffold { paddingValues ->
-            val navController = rememberNavController()
-            NavHost(navController = navController, startDestination = RandomMealScreen) {
-                composable<RandomMealScreen> {
-                    RandomMealScreen(
-                        paddingValues = paddingValues,
-                        onNavigateToCategories = { navController.navigate(CategoriesPage) },
-                    )
-                }
-                composable<CategoriesPage> {
-                    CategoriesPage(
-                        paddingValues = paddingValues,
+    KoinApplication(
+        application = { initKoin() }
+    ) {
+        WhatsCookingTheme {
+            Scaffold { paddingValues ->
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = RandomMealScreen) {
+                    composable<RandomMealScreen> {
+                        RandomMealScreen(
+                            paddingValues = paddingValues,
+                            onNavigateToCategories = { navController.navigate(CategoriesPage) },
+                        )
+                    }
+                    composable<CategoriesPage> {
+                        CategoriesPage(
+                            paddingValues = paddingValues,
 //                        onCategoryClick = { navController.navigate(it) }
-                    )
+                        )
+                    }
                 }
             }
         }
     }
+
 }
